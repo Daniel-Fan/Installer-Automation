5a8e5e99eeee34326286a5eed4a4a914e987834c
Daniel Fan <fanyuchensx@gmail.com>
2023-07-01 00:34:09 -0700

    Update scripts for all three branches

--- installer_bundle_scripts_sync.sh
+++ installer_bundle_scripts_sync.sh
@@ -18,18 +18,26 @@ cd "$temp_dir"
 git config user.name "Bedrock Bedrock Installer FID"
 git config user.email "bedrock.installer.fid@ibm.com"
 
-# Update the submodule
-git submodule update --init --recursive
-git submodule update --remote submodules/installer_scripts
+for branch in master release-cs323-ltsr release-ltsr22; do
+    # Checkout the branch
+    git checkout "$branch"
 
-# Check if the submodule has been updated
-if [[ $(git submodule status submodules/installer_scripts) == *"+"* ]]; then
-    timestamp=$(date +"%Y-%m-%d-%H-%M")
-    # Submodule has been updated, perform Git operations
-    git add .
-    git commit -m "installer-automation-$timestamp"
-    git push origin master
-fi
+    # Pull the latest code
+    git pull origin "$branch"
+
+    # Update the submodule
+    git submodule update --init --recursive
+    git submodule update --remote submodules/installer_scripts
+
+    # Check if the submodule has been updated
+    if [[ $(git submodule status submodules/installer_scripts) == *"+"* ]]; then
+        timestamp=$(date +"%Y-%m-%d-%H-%M")
+        # Submodule has been updated, perform Git operations
+        git add .
+        git commit -m "installer-automation-$timestamp"
+        git push origin "$branch"
+    fi
+done
 
 # Clean up the temporary directory
 rm -rf "$temp_dir"
